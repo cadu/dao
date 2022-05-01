d4126febd7fa6854ddbb0266a4327283bd518ed5
cadu <cadu@2km.com.br>
2022-05-01 15:04:26 +0100

    Initial commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x1815e786f8EB6001bC8F6850851F0FFDb20c459D";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
